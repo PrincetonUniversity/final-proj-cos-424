1495c322ff1b1e2d8a3ca3656c12827c0f0d02fe
Sean <panzixiang@gmail.com>
2016-05-08 03:17:48 -0400

    panda process

--- extracted/pandaprocess.py
+++ extracted/pandaprocess.py
@@ -10,44 +10,14 @@ global col
 
 def main():
     col = 0
-    parse('GSM142175', col)
-    col = 6
-    parse('GSM142176', col)
-    col = 16
-    parse('GSM142177', col)
-    col = 26
-    parse('GSM142178', col)
-    col = 36
-    parse('GSM142179', col)
-    col = 46
-    parse('GSM142180', col)
-    col = 56
-    parse('GSM142181', col)
-    col = 66
-    parse('GSM142182', col)
-    col = 76
-    parse('GSM142183', col)
-    col = 86
-    parse('GSM142184', col)
-    col = 96
-    parse('GSM142185', col)
-    col = 106
-    parse('GSM142186', col)
-    col = 116
-    parse('GSM142187', col)
-    col = 126
-    parse('GSM142188', col)
-    col = 136
-    parse('GSM142189', col)
-    col = 146
-    parse('GSM142190', col)
+    parse('GSM1421', col)
 
 
 def parse(prefix, col):
     first = True
     for file in glob.glob(prefix + '*.txt'):
         filename = os.path.basename(file)[:10]
-        print os.path.basename(file)
+        print filename
         with open(file, 'r') as csvfile:
 
             filedf = pd.read_csv(csvfile, sep='\t', header = 0)
