7ee0745d8557e57addda521b53a67925ea93f911
Sean <panzixiang@gmail.com>
2016-05-08 01:49:03 -0400

    second process

--- extracted/secondprocess.py
+++ extracted/secondprocess.py
@@ -7,23 +7,38 @@ import pandas as pd
 
 
 global globdf
-
 first = True
+filedf75 = pd.read_csv('GSM142175.csv', sep='\t', header = 0)
+filedf76 = pd.read_csv('GSM142176.csv', sep='\t', header = 0)
+filedf77 = pd.read_csv('GSM142177.csv', sep='\t', header = 0)
+filedf78 = pd.read_csv('GSM142178.csv', sep='\t', header = 0)
+filedf79 = pd.read_csv('GSM142179.csv', sep='\t', header = 0)
+filedf80 = pd.read_csv('GSM142180.csv', sep='\t', header = 0)
+filedf81 = pd.read_csv('GSM142181.csv', sep='\t', header = 0)
+filedf82 = pd.read_csv('GSM142182.csv', sep='\t', header = 0)
+filedf83 = pd.read_csv('GSM142183.csv', sep='\t', header = 0)
+filedf84 = pd.read_csv('GSM142184.csv', sep='\t', header = 0)
+filedf85 = pd.read_csv('GSM142185.csv', sep='\t', header = 0)
+filedf86 = pd.read_csv('GSM142186.csv', sep='\t', header = 0)
+filedf87 = pd.read_csv('GSM142187.csv', sep='\t', header = 0)
+filedf88 = pd.read_csv('GSM142188.csv', sep='\t', header = 0)
+filedf90 = pd.read_csv('GSM142190.csv', sep='\t', header = 0)
 
-for file in glob.glob('*.csv'):
-    print os.path.basename(file)
-    with open(file, 'r') as csvfile:
-        
-        filedf = pd.read_csv(csvfile, sep='\t', header = 0)
-
-        # processing one file #
-        if first:
-            globdf = filedf
-            first = False
-        else:
-            globdf = pd.merge(globdf, filedf, on='start', how='inner')
-        #print globdf.head()    
-            
+globdf = pd.merge(filedf75, filedf76, on='start', how='inner')
+globdf = pd.merge(globdf, filedf77, on='start', how='inner')
+globdf = pd.merge(globdf, filedf78, on='start', how='inner')
+globdf = pd.merge(globdf, filedf79, on='start', how='inner')
+globdf = pd.merge(globdf, filedf80, on='start', how='inner')
+globdf = pd.merge(globdf, filedf81, on='start', how='inner')
+globdf = pd.merge(globdf, filedf82, on='start', how='inner')
+globdf = pd.merge(globdf, filedf83, on='start', how='inner')
+globdf = pd.merge(globdf, filedf84, on='start', how='inner')
+globdf = pd.merge(globdf, filedf85, on='start', how='inner')
+globdf = pd.merge(globdf, filedf86, on='start', how='inner')
+globdf = pd.merge(globdf, filedf87, on='start', how='inner')
+globdf = pd.merge(globdf, filedf88, on='start', how='inner')
+globdf = pd.merge(globdf, filedf89, on='start', how='inner')
+globdf = pd.merge(globdf, filedf90, on='start', how='inner')
 
 # save
 globdf.to_csv('out.csv', sep='\t')        
