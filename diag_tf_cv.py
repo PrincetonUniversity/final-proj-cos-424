fixed gradient descent for mle

diag_tf_cv.py
@@ -127,7 +127,10 @@ def get_avg_r2_cv(K):
         if not IS_MLE:
           pred = argmax_exp(marginal_means, marginal_covs, marginal_alphas[0])[:len(unobserved_tested_ix)]
         else:
-          pred = gd_mle(marginal_means, marginal_covs, np.exp(marginal_alphas[0]), 5, EPS, warning='gd', verbose=False, minstep=1e-4)
+          print('means', np.any(np.isnan(marginal_means)), np.any(np.isfinite(marginal_means)))
+          print('covs', np.any(np.isnan(marginal_covs)), np.any(np.isfinite(marginal_covs)))
+          print('alphas', np.any(np.isnan(np.exp(marginal_alphas[0]))), np.any(np.isfinite(np.exp(marginal_alphas[0]))))
+          pred = gd_mle(marginal_means, marginal_covs, np.exp(marginal_alphas[0]), 50, EPS, warning='gd', verbose=False, minstep=1e-4)
           pred = pred.reshape(-1)[:len(unobserved_tested_ix)]
         actual = X_test[len(train_ix):len(train_ix)+len(unobserved_tested_ix)]
         rmse = sklearn.metrics.mean_squared_error(actual, pred)

utils/tf_gmm_em.py
@@ -168,15 +168,14 @@ class DoubleGDOptimizer(tf.train.GradientDescentOptimizer):
 
 # Given a GMM defined by KxD mus, KxD sigmas, and K alphas, returns the MLE estimate
 # by using gradient descent starting from each mu in mus
-def gd_mle(mus, sigmas, alphas, nsteps, tol, warning=None, verbose=False, minstep=1e-1):
+def gd_mle(mus, sigmas, alphas, nsteps, tol, warning=None, verbose=False, minstep=1e-3):
     # Use GD from each of the means with a step size less than the min distance between those means
     # step size is min(mean diff) * min(mean likelihoods) / sum(adj likelihoods)
     mudist = sklearn.metrics.pairwise.pairwise_distances(mus, metric='l2')
-    alphasM = alphas.reshape(-1, 1)
-    alphadist = sklearn.metrics.pairwise.pairwise_distances(alphasM, metric='l1')
-    min_alpha = np.minimum(np.tile(alphasM, len(alphas)), alphas)
+    alphasM = np.tile(alphas, (len(alphas), 1)).transpose()
+    alpha_sum = np.array([[i+j for j in alphas] for i in alphas])
     steps_per_decay = max(nsteps // 100, 1)
-    step_size = mudist * min_alpha / alphadist / steps_per_decay
+    step_size = mudist * alphasM / alpha_sum / max(steps_per_decay, 2)
     np.fill_diagonal(step_size, np.inf)
     step_size = step_size.min(axis=1)
     best_ll = np.inf, None