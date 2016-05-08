505a0e1de0ff6816697761f8ff712dbe24e184de
Sean <panzixiang@gmail.com>
2016-05-08 01:56:57 -0400

    second process

--- extracted/secondprocess.py
+++ extracted/secondprocess.py
@@ -25,20 +25,35 @@ filedf88 = pd.read_csv('GSM142188.csv', sep='\t', header = 0)
 filedf90 = pd.read_csv('GSM142190.csv', sep='\t', header = 0)
 
 globdf = pd.merge(filedf75, filedf76, on='start', how='inner')
+print '75,76'
 globdf = pd.merge(globdf, filedf77, on='start', how='inner')
+print '77'
 globdf = pd.merge(globdf, filedf78, on='start', how='inner')
+print '79'
 globdf = pd.merge(globdf, filedf79, on='start', how='inner')
+print '79'
 globdf = pd.merge(globdf, filedf80, on='start', how='inner')
+print '80'
 globdf = pd.merge(globdf, filedf81, on='start', how='inner')
+print '81'
 globdf = pd.merge(globdf, filedf82, on='start', how='inner')
+print '82'
 globdf = pd.merge(globdf, filedf83, on='start', how='inner')
+print '83'
 globdf = pd.merge(globdf, filedf84, on='start', how='inner')
+print '84'
 globdf = pd.merge(globdf, filedf85, on='start', how='inner')
+print '85'
 globdf = pd.merge(globdf, filedf86, on='start', how='inner')
+print '86'
 globdf = pd.merge(globdf, filedf87, on='start', how='inner')
+print '87'
 globdf = pd.merge(globdf, filedf88, on='start', how='inner')
+print '88'
 globdf = pd.merge(globdf, filedf89, on='start', how='inner')
+print '89'
 globdf = pd.merge(globdf, filedf90, on='start', how='inner')
+print '90'
 
 # save
 globdf.to_csv('out.csv', sep='\t')        
