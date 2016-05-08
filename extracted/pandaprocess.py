84abb1318f35190371ae9d5955770cf42a0d7488
Sean <panzixiang@gmail.com>
2016-05-08 01:29:44 -0400

    split

--- extracted/pandaprocess.py
+++ extracted/pandaprocess.py
@@ -5,41 +5,60 @@ import itertools
 import pandas as pd
 #os.chdir("/mydir")
 
-
-global globdf
-
-first = True
+global col 
 col = 0
-for file in glob.glob('GSM14217*.txt'):
-    print os.path.basename(file)
-    with open(file, 'r') as csvfile:
 
-        filedf = pd.read_csv(csvfile, sep='\t', header = 0)
+def main():
+    parse('GSM142175')
+    parse('GSM142176')
+    parse('GSM142177')
+    parse('GSM142178')
+    parse('GSM142179')
+    parse('GSM142180')
+    parse('GSM142181')
+    parse('GSM142182')
+    parse('GSM142183')
+    parse('GSM142184')
+    parse('GSM142185')
+    parse('GSM142186')
+    parse('GSM142187')
+    parse('GSM142188')
+    parse('GSM142189')
+    parse('GSM142190')
 
-        # processing one file #
 
-        # take chr1
-        chr1df = filedf.loc[filedf['chr'] == 'chr1']
-        
-        # add ratio
-        ratio = ((chr1df.MethylatedReadCount)+(chr1df.ConcordantMethylatedReadCount)) / \
-                ((chr1df.MethylatedReadCount)+(chr1df.UnmethylatedReadCount)+(chr1df.ConcordantMethylatedReadCount)+(chr1df.ConcordantUnmethylatedReadCount))
+def parse(prefix)
+    first = True
+    for file in glob.glob(prefix + '*.txt'):
+        print os.path.basename(file)
+        with open(file, 'r') as csvfile:
 
-        chr1df['ratio' + str(col)] = ratio
+            filedf = pd.read_csv(csvfile, sep='\t', header = 0)
 
-        chr1df = chr1df[['start', 'ratio' + str(col)]]
-        if first:
-            globdf = chr1df
-            first = False
-        else:
-            globdf = pd.merge(globdf, chr1df, on='start', how='inner')
+            # processing one file #
 
-        col = col +1
-        # print col
-        #print globdf.head()    
+            # take chr1
+            chr1df = filedf.loc[filedf['chr'] == 'chr1']
             
-# save
-globdf.to_csv('out7.csv', sep='\t')        
+            # add ratio
+            ratio = ((chr1df.MethylatedReadCount)+(chr1df.ConcordantMethylatedReadCount)) / \
+                    ((chr1df.MethylatedReadCount)+(chr1df.UnmethylatedReadCount)+(chr1df.ConcordantMethylatedReadCount)+(chr1df.ConcordantUnmethylatedReadCount))
+
+            chr1df['ratio' + str(col)] = ratio
+
+            chr1df = chr1df[['start', 'ratio' + str(col)]]
+            if first:
+                globdf = chr1df
+                first = False
+            else:
+                globdf = pd.merge(globdf, chr1df, on='start', how='inner')
+
+            col = col +1
+            # print col
+            #print globdf.head()    
+                
+    # save
+    globdf.to_csv(prefix + '.csv', sep='\t')        
 
 
         
